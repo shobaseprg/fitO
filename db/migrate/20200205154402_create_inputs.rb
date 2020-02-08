4d4e492eeeb95dcc83ad56550223a39d98b43fee
shobaseprg <pgymlsho19128@gmail.com>
2020-02-08 13:44:44 +0900

    Revert "Revert "create_input&lessons""
    This reverts commit b96277dec36c513508fd91d18a98c0cb65b9f837.

--- db/migrate/20200205154402_create_inputs.rb
+++ db/migrate/20200205154402_create_inputs.rb
@@ -1,7 +1,9 @@
 class CreateInputs < ActiveRecord::Migration[5.2]
   def change
     create_table :inputs do |t|
-
+      t.text :title
+      t.text :content 
+      t.text :image
       t.timestamps
     end
   end
