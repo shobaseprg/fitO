fae198dc8881da1fa9865aa0960b145e21be648e
shobaseprg <pgymlsho19128@gmail.com>
2020-02-09 14:54:05 +0900

    fix_migrate(output)
    output付近でマイグレーションにエラーがでたのでマイグレーションリセットをかけて、再度マイグレートしなおした

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_053701) do
+ActiveRecord::Schema.define(version: 2020_02_09_053015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "all_inputs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "all_outputs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
