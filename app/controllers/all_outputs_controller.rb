fdafc938ea084b4935fc51ebd3b8a4c7a96b9da5
shobaseprg <pgymlsho19128@gmail.com>
2020-02-15 03:34:44 +0900

    each_do

--- app/controllers/all_outputs_controller.rb
+++ app/controllers/all_outputs_controller.rb
@@ -1,2 +1,6 @@
 class AllOutputsController < ApplicationController
+  def index
+    @lessonskone = Lesson.where(k_or_o: 1).where(lesson_no: 1)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'entrances#index'
+  resources :all_outputs, only: [:index]
+  resources :lessons do
+    resources :inputs , only: [:index]
   end
 end
+
