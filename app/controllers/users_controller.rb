099c5670e7fe3f2ba59be33287e3ad11d394e84b
shobaseprg <pgymlsho19128@gmail.com>
2020-02-15 05:57:36 +0900

    mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def show
+    @myinputs = current_user.inputs.where(input_or_output: 1)
+    @myoutputs = current_user.inputs.where(input_or_output: 2)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'entrances#index'
   resources :all_outputs, only: [:index]
+  resources :users, only: :show
   resources :lessons do
     resources :inputs , only: [:index, :new, :create]
   end
