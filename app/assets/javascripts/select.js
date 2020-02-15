b9411f0643a6e679334b257fd9a0fc7a52a32556
shobaseprg <pgymlsho19128@gmail.com>
2020-02-15 20:22:42 +0900

    reset

--- app/assets/javascripts/select.js
+++ app/assets/javascripts/select.js
@@ -11,7 +11,7 @@ $(function() {
     $(".input-base-dev-btns")[0].classList.remove("Show");
   };
 
-  $(".input-btn").click(showInputBtns);
+  $(".base-btn").click(showInputBtns);
   $(".output-btn").click(showOutputBtns);
 
 
