0751cea44c38a02c81fc53445437d38f8b4673e9
shobaseprg <pgymlsho19128@gmail.com>
2020-02-17 14:38:56 +0900

    lesson-swich

--- app/assets/javascripts/select.js
+++ app/assets/javascripts/select.js
@@ -1,6 +1,7 @@
 $(function() {
   // ファーストボタンの二つを変数化
 let firstBtns = $(".first-btn");
+let secondBtns = $(".secondBtn");
 
   function firstBtnSwith(){
     $(".active-btn").removeClass("active-btn");
@@ -19,10 +20,22 @@ let firstBtns = $(".first-btn");
     $(".secondBtns").eq(index).addClass("secondBtns-show");
     // クリックされたthisと同じ配列番号のsecondBtnsクラスにsecondBtns-showクラスを与える。
     // つまり表示される。
-
   }
+
+function lessonSwith(){
+  // デフォルト画面削除用
+  $(".defultLesson").removeClass("active-defult-lesson").addClass("non-defult-lesson");
+
+  $(".lessons-show").removeClass("lessons-show");
+
+  const index = secondBtns.index(this);
+
+  $(".lessonsAllBox").eq(index).addClass("lessons-show");
+}
+
 
   // ファストボタン起動用
   firstBtns.click(firstBtnSwith);
-
+  // セカンドボタン起動用
+  secondBtns.click(lessonSwith);
 });
