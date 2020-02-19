286c828e987debff89382d222ecf251afca31f21
shobaseprg <pgymlsho19128@gmail.com>
2020-02-19 12:32:05 +0900

    mypage-input-output-show

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -2,10 +2,27 @@ $(function() {
 let mypageBtns = $(".mypage-btn");
 
   function mypageBtnSwith(){
-    $(".active-btn").removeClass("active-btn");
+    $(".active-btn").removeClass("active-btn")
     $(this).addClass("active-btn");
+
+    $(".mypageDefault").removeClass("mypageDefault");
   }
 
-mypageBtns.click(mypageBtnSwith);
+  function showMyinput(){
+$(".mypageInput").removeClass("mypageNonShow");
+$(".mypageOutput").addClass("mypageNonShow");
+  }
 
+  function showMyOutput(){
+    $(".mypageOutput").removeClass("mypageNonShow");
+$(".mypageInput").removeClass("mypageNonShow");
+$(".mypageInput").addClass("mypageNonShow");
+      }
+
+  
+
+  mypageBtns.click(mypageBtnSwith);
+
+  $(".mypageInputBtn").click(showMyinput)
+  $(".mypageOutputBtn").click(showMyOutput)
 });
