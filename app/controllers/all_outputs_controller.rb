b8f3a853b22bbdd4454a08f0c18e42385140ed0a
shobaseprg <pgymlsho19128@gmail.com>
2020-02-22 14:14:02 +0900

    lesson_increase

--- app/controllers/all_outputs_controller.rb
+++ app/controllers/all_outputs_controller.rb
@@ -2,11 +2,20 @@ class AllOutputsController < ApplicationController
   def index
     lessonsBases_of_1 = Lesson.where(k_or_o: 1).where(lesson_no:1)
     lessonsBases_of_2 = Lesson.where(k_or_o: 1).where(lesson_no:2)
+    lessonsBases_of_3 = Lesson.where(k_or_o: 1).where(lesson_no:3)
+    lessonsBases_of_4 = Lesson.where(k_or_o: 1).where(lesson_no:4)
+    lessonsBases_of_5 = Lesson.where(k_or_o: 1).where(lesson_no:5)
+    lessonsBases_of_6 = Lesson.where(k_or_o: 1).where(lesson_no:6)
+
     # 基礎レッスンをそれぞれlessonごとに格納
 
     @lessonsBases = [
                     lessonsBases_of_1,
-                    lessonsBases_of_2
+                    lessonsBases_of_2,
+                    lessonsBases_of_3,
+                    lessonsBases_of_4,
+                    lessonsBases_of_5,
+                    lessonsBases_of_6
                     ]
     # 基礎の各レッスンを、@lessonsBasesに配列で格納
 
